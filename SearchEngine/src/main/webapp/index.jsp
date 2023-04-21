7a9bad8e8ac371daec39dc6fd2b8190444ec0aec
prabhat7679 <118530788+prabhat7679@users.noreply.github.com>
2023-04-21 06:47:08 +0530

    Add files via upload

---
+++ SearchEngine/src/main/webapp/index.jsp
@@ -0,0 +1,15 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<h1>Simple Search Engine</h1>
+<form action ="Search">
+  <input type = "text" name = "keyword"></input>
+  <button type = "Submit">Search</button>
+</form>
+<form action = "History">
+     <button type ="submit">History</button>
+     </form>
+</body>
+</html>
---
+++ WebCrawler/src/main/java/org/example/Indexer.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.jsoup.nodes.Document;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Indexer {
+    static Connection connection=null;
+    Indexer(Document document,String url){
+      // select important element
+        String title = document.title();
+        String link= url;
+        String text = document.text();
+        try {
+            connection = DataBaseConnection.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("Insert into pages values(?,?,?);");
+            preparedStatement.setString(1, title);
+            preparedStatement.setString(2, link);
+            preparedStatement.setString(3, text);
+            preparedStatement.executeUpdate();
+        }
+        catch (SQLException sqlException){
+            sqlException.printStackTrace();
+        }
+    }
+}
